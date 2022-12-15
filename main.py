4f412d3da2ee9f6c5d23049eb92f99e15beb12c3
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2022-12-15 20:54:23 +0100

    ejercicio 4

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import exercici1
-
+import exercici4_RaulVaquerizo
 #Raul Vaquerizo
 print('Ejercicio 1 hecho por Raul Vaquerizo')
 print('Array diagonal desde 0 hasta 49')
 print(exercici1.diagonal())
+ 
+#Ejercicio 4 Raul Vaquerizo
+print("Ejercicio 4")
+print(exercici4_RaulVaquerizo.exercici4Rand)
